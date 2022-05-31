a078111fafc9bf81b6c24668530e968bf7ccf41d
Farkhodjon Khojikurbonov <farhodand92@gmail.com>
2022-05-31 18:24:32 +0500

    update

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ if file_img:
     st.success(f"Predict: {pred}")
     st.info(f"Probability: {probs[pred_id]*100:.1f}%")
     # plotting
-    fig = px.bar(x=probs*100, y=model.dls.vocab, width=100)
+    fig = px.bar(x=probs*100, y=model.dls.vocab, width=200, height=200)
     st.plotly_chart(fig)
